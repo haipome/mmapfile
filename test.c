cbaa97b48faf03565226ef4fef238e4c1e0cae44
haipo yang <yang@haipo.me>
2012-03-06 18:18:59 +0800

    test

--- test.c
+++ test.c
@@ -4,9 +4,10 @@
 int main(int argc, char **argv)
 {
 	if (argc != 2) {
-		fprintf(stderr, "usage: mmage filename\n");
+		fprintf(stderr, "usage: mmap filename\n");
 		return -1;
 	}
+	
 	struct mmap_f f;
 	mmap_f_open(argv[1], &f);
 	
